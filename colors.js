89202ad387fb27c8870ff65ffb7231f63e411c50
Guillermo <gui@mit.edu>
2020-03-02 23:01:39 -0800

    add smaller splash image, capitalize links on hom epage

--- colors.js
+++ colors.js
@@ -1,7 +1,7 @@
 ;[].slice.call(document.querySelectorAll('path')).forEach(function(p) {
-	p.addEventListener('mouseover', function() {
-		p.style.fill =
-			'#' +
-			('00000' + ((Math.random() * (1 << 24)) | 0).toString(16)).slice(-6)
-	})
+	function color() {
+		p.style.fill = '#' + ('00000' + ((Math.random() * (1 << 24)) | 0).toString(16)).slice(-6)
+	}
+	p.addEventListener('mouseover', color)
+	p.addEventListener('click', color)
 })
