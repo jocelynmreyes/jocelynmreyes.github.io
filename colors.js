4fb6b8ec4a02d8779445751454a3c31d79d71be1
Guillermo <gui@mit.edu>
2020-08-06 12:56:14 -0700

    switch info pages to white

--- colors.js
+++ colors.js
@@ -1,7 +1,17 @@
-;[].slice.call(document.querySelectorAll('path')).forEach(function(p) {
+;[].slice.call(document.querySelectorAll('path')).forEach(function (p) {
 	function color() {
 		p.style.fill = '#' + ('00000' + ((Math.random() * (1 << 24)) | 0).toString(16)).slice(-6)
 	}
-	p.addEventListener('mouseover', color)
-	p.addEventListener('click', color)
+	var hasMoused = false
+	function mouseColor() {
+		hasMoused = true
+		color()
+	}
+
+	p.addEventListener('mouseover', mouseColor)
+	p.addEventListener('click', mouseColor)
+
+	setInterval(function () {
+		if (!hasMoused && Math.random() > 0.95) color()
+	}, 500)
 })
