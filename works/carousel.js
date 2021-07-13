7cde89868e509284f638eb46790f52964190f820
Jocelyn Reyes <reyesmjocelyn@gmail.com>
2021-07-12 21:31:31 -0700

    remove carousel

---
+++ works/carousel.js
@@ -0,0 +1,19 @@
+// function slick(el){
+//     $(el).slick({
+//         // adaptiveHeight: true,
+//         centerMode: true,
+//         variableWidth: true,
+//         focusOnSelect: true,
+        
+//         responsive: [
+//             {
+//                 breakpoint: 1024,
+//                 settings: "unslick"
+//             },
+//         ]
+//     })
+// }
+// ;[].slice.call(document.querySelectorAll('.first')).forEach(function(img){
+//     if(img.complete) slick(img.parentElement)
+//     else img.onload = slick.bind(null, img.parentElement)
+// })
